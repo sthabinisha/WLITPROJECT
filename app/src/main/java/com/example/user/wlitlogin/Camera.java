ec1196ea5d3e9f6bbb1172aa5ad0b5a0d94cbb60
sthabinisha <shrestha.binishaa@gmail.com>
2017-02-27 15:13:16 +0545

    image capture and uploat to server

--- app/src/main/java/com/example/user/wlitlogin/Camera.java
+++ app/src/main/java/com/example/user/wlitlogin/Camera.java
@@ -1,94 +1,230 @@
 package com.example.user.wlitlogin;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.net.sip.SipAudioCall;
-
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
-
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Camera extends AppCompatActivity {
-    ImageView iv, imgPreview;
-    private Uri fileUri; // file url to store image/video
+public class Camera extends Activity {
+
+    String picturePath;
+    Uri selectedImage;
+    Bitmap photo;
+    // LogCat tag
+    private static final String TAG = Camera.class.getSimpleName();
+
+
+    // Camera activity request codes
     private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
+
+
     public static final int MEDIA_TYPE_IMAGE = 1;
-    private static final String IMAGE_DIRECTORY_NAME = " kapas";
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    private Uri fileUri; // file url to store image/video
+
+    private ImageButton btnCapturePicture;
+    ImageView imageHolder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        iv = (ImageView) findViewById(R.id.imageView3);
-        imgPreview = (ImageView) findViewById(R.id.imgPreview);
 
-        Button btnCancel = (Button) findViewById(R.id.button_camera);
-     ImageButton btnClick = (ImageButton) findViewById(R.id.img_press);
+        // Changing action bar background color
+        // These two lines are not needed
 
-        //Set listener on Capture button
-        btnClick.setOnClickListener(new View.OnClickListener() {
+        btnCapturePicture = (ImageButton) findViewById(R.id.img_press);
+        imageHolder= (ImageView) findViewById(R.id.imageView3);
+
+
+        /**
+         * Capture image button click event
+         */
+        btnCapturePicture.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-//                Intent c = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //Implicit Intent
+                // capture picture
+                captureImage();
+            }
+        });
+
+        /**
+         * Record video button click event
+         */
+
+
+        // Checking camera availability
+        if (!isDeviceSupportCamera()) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Your device doesn't support camera",
+                    Toast.LENGTH_LONG).show();
+            // will close the app if the device does't have camera
+            finish();
+        }
+    }
+
+    /**
+     * Checking device has camera hardware or not
+     * */
+    private boolean isDeviceSupportCamera() {
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /**
+     * Launching camera app to capture image
+     */
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    /**
+     * Launching camera app to record video
+     */
+
+
+    /**
+     * Here we store the file url as it will be null after returning from camera
+     * app
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save file url in bundle as it will be null on screen orientation
+        // changes
+        outState.putParcelable("file_uri", fileUri);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // get the file url
+        fileUri = savedInstanceState.getParcelable("file_uri");
+        System.out.println(fileUri+"I am here");
+    }
+
+
+
+    /**
+     * Receiving activity result method will be called after closing the camera
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+
+            if (resultCode == RESULT_OK) {
+                if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+
+                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+                    imageHolder.setImageBitmap(bitmap);
 //
-//                startActivityForResult(c, 0);
+                    Uri tempUri = getImageUri(getApplicationContext(),bitmap);
+                    File finalFile = new File(getRealPathFromURI(tempUri));
+                    System.out.println(finalFile+"ok here");
+                    launchUploadActivity(finalFile,true);
 
 
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            } else if (resultCode == RESULT_CANCELED) {
 
-                fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
 
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-
-                // start the image capture Intent
-                startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
             }
-        });
-
-
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent j = new Intent(Camera.this,HomePage.class);
-                startActivity(j);
-            }
-        });
-
+        }
+    }
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 
-    private Uri getOutputMediaFileUri(int mediaTypeImage) {
-//        return Uri.fromFile(getOutputMediaFile(mediaTypeImage));
-        Uri photoURI = FileProvider.getUriForFile(Camera.this, "com.example.user.wlitlogin.Camera" + ".provider", getOutputMediaFile(mediaTypeImage));
-        return photoURI;
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
     }
 
-    private File getOutputMediaFile(int type) {
+    private void launchUploadActivity(File finalFile, boolean isImage){
+        System.out.println(finalFile+"ok here 2");
+        Intent i = new Intent(Camera.this, UploadActivity.class);
+        i.putExtra("filePath", finalFile);
+        i.putExtra("isImage", isImage);
+        startActivity(i);
+    }
+
+    /**
+     * ------------ Helper Methods ----------------------
+     * */
+
+    /**
+     * Creating file uri to store image/video
+     */
+    public Uri getOutputMediaFileUri(int type) {
+//        return Uri.fromFile(getOutputMediaFile(type));
+        File fileToReturn =  getOutputMediaFile(type);
+//        return  fileToReturn!=null?Uri.fromFile(fileToReturn):
+//                null;
+        return FileProvider.getUriForFile(Camera.this, Camera.this.getApplicationContext().getPackageName() + ".provider", getOutputMediaFile(type));
+////
+    }
+
+
+    /**
+     * returning image / video
+     */
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
         File mediaStorageDir = new File(
                 Environment
-                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                IMAGE_DIRECTORY_NAME);
+                        .getExternalStorageDirectory(),
+                Config.IMAGE_DIRECTORY_NAME);
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
-                        + IMAGE_DIRECTORY_NAME + " directory");
+                Log.d(TAG, "Oops! Failed create "
+                        + Config.IMAGE_DIRECTORY_NAME + " directory");
                 return null;
             }
         }
@@ -98,7 +234,7 @@ public class Camera extends AppCompatActivity {
                 Locale.getDefault()).format(new Date());
         File mediaFile;
         if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+            mediaFile = new File(mediaStorageDir, File.separator
                     + "IMG_" + timeStamp + ".jpg");
         } else {
             return null;
@@ -106,73 +242,4 @@ public class Camera extends AppCompatActivity {
 
         return mediaFile;
     }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        // save file url in bundle as it will be null on scren orientation
-        // changes
-        outState.putParcelable("file_uri", fileUri);
-    }
-
-    /*
-     * Here we restore the fileUri again
-     */
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-        // get the file url
-        fileUri = savedInstanceState.getParcelable("file_uri");
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-//        Bitmap m = (Bitmap) data.getExtras().get("data");
-//        iv.setImageBitmap(m);
-//        iv.setScaleType(ImageView.ScaleType.FIT_XY);
-//        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
-//            if (resultCode == RESULT_OK) {
-                if (resultCode == RESULT_OK) {
-                    if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
-                // successfully captured the image
-                // display it in image view
-                previewCapturedImage();
-            } else if (resultCode == RESULT_CANCELED) {
-                // user cancelled Image capture
-                Toast.makeText(getApplicationContext(),
-                        "User cancelled image capture", Toast.LENGTH_SHORT)
-                        .show();
-            } else {
-                // failed to capture image
-                Toast.makeText(getApplicationContext(),
-                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
-    }
-
-    private void previewCapturedImage() {
-        try {
-            // hide video preview
-
-            imgPreview.setVisibility(View.VISIBLE);
-
-            // bimatp factory
-            BitmapFactory.Options options = new BitmapFactory.Options();
-
-            // downsizing image as it throws OutOfMemory Exception for larger
-            // images
-            options.inSampleSize = 8;
-
-            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
-                    options);
-
-            imgPreview.setImageBitmap(bitmap);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-    }
-    }
-
+}
---
+++ app/src/main/java/com/example/user/wlitlogin/Config.java
@@ -0,0 +1,9 @@
+package com.example.user.wlitlogin;
+
+public class Config {
+	// File upload url (replace the ip with your server address)
+	public static final String FILE_UPLOAD_URL = "http://kapas.wlit.org.np/fileUpload.php";
+	
+	// Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "kapas";
+}
